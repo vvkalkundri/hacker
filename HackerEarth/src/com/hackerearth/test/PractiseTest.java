c647060f4098659092bcc2be401a7ee2e049ae17
vvkalkundri <vvkalkundri@gmail.com>
2014-09-14 03:42:34 +0530

    this is a commit from eclipse

--- HackerEarth/src/com/hackerearth/test/PractiseTest.java
+++ HackerEarth/src/com/hackerearth/test/PractiseTest.java
@@ -2,6 +2,6 @@ package com.hackerearth.test;
 
 public class PractiseTest {
 	public static void main(String[] args) {
-		System.out.println("THis is a test");
+		System.out.println("THis is a test java program");
 	}
 }
